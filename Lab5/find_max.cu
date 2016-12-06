d3708e3b22f20dd76841cd47e7b3cd515c1c8efb
Matthieu Zins <matzi633@southfork-01.edu.isy.liu.se>
2016-12-06 20:19:48 +0100

    finished find_max

--- Lab5/find_max.cu
+++ Lab5/find_max.cu
@@ -1,14 +1,41 @@
 // Reduction lab, find maximum
 
 #include <stdio.h>
+
 #include "milli.h"
 
+
+
+/* first kernel binary search in each block */
 __global__ void find_max(int *data, int N)
 {
   int i;
-  i = threadIdx.x + blockDim.x*blockIdx.x;
+  i = threadIdx.x * 2 + blockDim.x*blockIdx.x*2;
 
-	// Write your CUDA kernel here
+  int c = 2;
+
+  while(c <= blockDim.x*2)
+  {
+	if(i%c == 0)
+	{
+		data[i] = data[i]>data[i+c/2] ? data[i] : data[i+c/2];
+	}
+	c*=2;
+	__syncthreads();
+  }
+}
+
+/* second kernel to find the max among the maximum of each block */
+__global__ void find_max_among_blocks(int *data,int blockSize, int nbBlocks)
+{
+	for(int i=0; i<nbBlocks; ++i)
+	{
+		if(data[0]<data[i*blockSize])
+			data[0] = data[i*blockSize];
+	}
+
+	
+// Write your CUDA kernel here
 }
 
 void launch_cuda_kernel(int *data, int N)
@@ -21,9 +48,13 @@ void launch_cuda_kernel(int *data, int N)
 	cudaMemcpy(devdata, data, size, cudaMemcpyHostToDevice );
 	
 	// Dummy launch
-	dim3 dimBlock( 8, 1 );
-	dim3 dimGrid( 8, 1 );
+	dim3 dimBlock( 1024, 1 );
+	dim3 dimGrid( N / 2048, 1 );
+	dim3 dimBlock2(1,1);
+	dim3 dimGrid2(1,1);
 	find_max<<<dimGrid, dimBlock>>>(devdata, N);
+
+	find_max_among_blocks<<<dimGrid2, dimBlock2>>>(devdata, 2048, N/2048);
 	cudaError_t err = cudaPeekAtLastError();
 	if (err) printf("cudaPeekAtLastError %d %s\n", err, cudaGetErrorString(err));
 
@@ -46,14 +77,25 @@ void find_max_cpu(int *data, int N)
 	data[0] = m;
 }
 
-#define SIZE 1024
-//#define SIZE 16
+//#define SIZE 1024
+#define SIZE 2048 * 16384 * 2
 // Dummy data in comments below for testing
-int data[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
+int data[SIZE];//= {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
 int data2[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
 
 int main()
 {
+
+
+	/*struct cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop,0);
+	
+	printf("max threads pr blocks : %d\n",prop.maxThreadsPerBlock);
+	printf("max threads dim : %d %d %d\n",prop.maxThreadsDim[0],prop.maxThreadsDim[1],prop.maxThreadsDim[2]);
+	printf("max grid dim : %d %d %d\n",prop.maxGridSize[0],prop.maxGridSize[1],prop.maxGridSize[2]);*/
+
+
+
   // Generate 2 copies of random data
   srand(time(NULL));
   for (long i=0;i<SIZE;i++)
---
+++ Lab5/test.cu
@@ -0,0 +1,95 @@
+// Reduction lab, find maximum
+
+#include <stdio.h>
+#include "milli.h"
+
+
+#define SIZE 102400000
+
+
+__global__ void find_max(uint *data, uint intsPerThread) {
+  uint index = threadIdx.x + blockDim.x * blockIdx.x;
+	uint m = data[index];
+	for (uint i = 0; i < intsPerThread; i++)
+		if (data[index + i] > m)
+			m = data[index + i];
+  data[index] = m;
+
+	__syncthreads();
+
+	if (threadIdx.x == 0) {
+		for (uint i = 0; i < blockDim.x; i++)
+			if (data[index + i * intsPerThread] > m)
+				m = data[index + i * intsPerThread];
+		data[index] = m;
+	}
+}
+
+void find_max_between_blocks(uint *data, uint intsPerThread, uint gridSize, uint blockSize) {
+	uint m = data[0];
+	for (uint i = 0; i < gridSize; i++)
+		if (data[i * blockSize * intsPerThread] > m)
+			m = data[i * blockSize * intsPerThread];
+	data[0] = m;
+}
+
+void launch_cuda_kernel(uint *data, uint N) {
+	uint *devdata;
+	uint size = sizeof(int) * N;
+	cudaMalloc( (void**)&devdata, size);
+	cudaMemcpy(devdata, data, size, cudaMemcpyHostToDevice );
+
+	dim3 dimBlock(16, 1);
+	dim3 dimGrid(8, 1);
+	dim3 oneGrid(1, 1);
+	uint intsPerThread = SIZE / 8 / 16;
+	find_max<<<dimGrid, dimBlock>>>(devdata, intsPerThread);
+	cudaThreadSynchronize();
+	cudaError_t err = cudaPeekAtLastError();
+	if (err) printf("cudaPeekAtLastError %d %s\n", err, cudaGetErrorString(err));
+
+	// Only the result needs copying!
+	cudaMemcpy(data, devdata, sizeof(int), cudaMemcpyDeviceToHost );
+	cudaFree(devdata);
+
+
+}
+
+// CPU max finder (sequential)
+void find_max_cpu(uint *data, uint N) {
+  uint i, m;
+
+	m = data[0];
+	for (i = 0; i < N; i++) {
+		if (data[i] > m)
+			m = data[i];
+	}
+	data[0] = m;
+}
+
+// Dummy data in comments below for testing
+uint data[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
+uint data2[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
+
+int main() {
+  // Generate 2 copies of random data
+  srand(time(NULL));
+  for (long i=0;i<SIZE;i++) {
+    data[i] = rand() % (SIZE * 5);
+    data2[i] = data[i];
+  }
+
+  // The GPU will not easily beat the CPU here!
+  // Reduction needs optimizing or it will be slow.
+  ResetMilli();
+  find_max_cpu(data, SIZE);
+  printf("CPU time %f\n", GetSeconds());
+  ResetMilli();
+  launch_cuda_kernel(data2, SIZE);
+  printf("GPU time %f\n", GetSeconds());
+
+  // Print result
+  printf("\n");
+  printf("CPU found max %d\n", data[0]);
+  printf("GPU found max %d\n", data2[0]);
+}
